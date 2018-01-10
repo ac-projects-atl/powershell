6c1122946fe98069aa971e5f4adaef9211b3d84f
ac-projects-atl <35309450+ac-projects-atl@users.noreply.github.com>
2018-01-10 14:36:58 -0500

    Create o365_remote.ps1

---
+++ o365_remote.ps1
@@ -0,0 +1,4 @@
+﻿Set-ExecutionPolicy RemoteSigned
+$cred = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $cred -Authentication Basic –AllowRedirection
+Import-PSSession $Session
